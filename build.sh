fbcf885603088f18bd0605308c92881021257088
Christian Hoppner <chris@mkaito.net>
2022-01-19 07:30:48 +0000

    Fix planet gen defs, adjust ore maps for Pertam
    Planet generation definitions were not being read correctly for,
    at least, Alien. Fix by importing many Better Stone defs whole-sale.
    
    Pertam should now have slightly less spammy Cobalt, and slightly
    less Unicornian Magnesium

--- build.sh
+++ build.sh
@@ -9,28 +9,23 @@ GAMEMODS='/mnt/c/Users/chris/AppData/Roaming/SpaceEngineers/Mods'
 
 # Generate ore maps
 # NOTE: Runs the windows version of java because reasons
-pushd Procedural_Ore_Generator
-  java.exe -Xms512M -Xmx8G -jar Procedural_Ore_Generator.jar
-popd
+# pushd Procedural_Ore_Generator
+#   java.exe -Xms2G -Xmx16G -jar Procedural_Ore_Generator.jar
+# popd
 
 # Reset output folder
 rm -rf ./Upload
 mkdir -p ./Upload/Data
 
 # Base game data files
-rsync -av \
-	--filter='- **/*Tutorial/' \
-	--filter='- **/SystemTestMap/' \
-	"$GAMEDATA/PlanetDataFiles" ./Upload/Data/
+rsync -av "$GAMEDATA/PlanetDataFiles" ./Upload/Data/
 
 # POG Output
-rsync -av \
-	--filter='- **/*_coloured.png' \
-	Procedural_Ore_Generator/PlanetDataFiles ./Upload/Data/
+rsync -av --exclude='*_coloured.png' \
+  Procedural_Ore_Generator/PlanetDataFiles ./Upload/Data/
 
 cp -v Procedural_Ore_Generator/PlanetGeneratorDefinitions.sbc ./Upload/Data
-cp -v Procedural_Ore_Generator/Triton.sbc ./Upload/Data
-cp -v Procedural_Ore_Generator/Pertam.sbc ./Upload/Data
+cp -v Procedural_Ore_Generator/*.sbc ./Upload/Data
 
 # Manual overrides
 rsync -aL ./Output/ ./Upload/
