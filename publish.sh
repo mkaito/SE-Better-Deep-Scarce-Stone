45d0e5b214291e8b54e30bd85bcf6e16a0b52867
Christian Höppner <chris@mkaito.net>
2022-01-24 18:47:50 +0000

    Auto-generate workshop description with pandoc

--- publish.sh
+++ publish.sh
@@ -3,12 +3,23 @@ set -euo pipefail
 IFS=$'\n\t'
 
 rm -f mod.vdf
+rm -f description.bb
+
+pandoc -f org -t ./vendor/2bbcode/bbcode_steam.lua <readme.org >|description.bb
+
+description_size=$(wc -c < description.bb)
+if [[ $description_size -gt 7999 ]]; then
+  echo "Description size too big: $description_size. Limit 8000."
+  exit 1
+fi
+
 cat << EOF > mod.vdf
 "workshopitem"
 {
   "appid"             "244850"
   "publishedfileid"   "2724252237"
   "contentfolder"     "$PWD/Upload"
+  "description"       "$(cat $PWD/description.bb)"
 }
 EOF
 
